c5e4452229ac9766216c6e56f6753a578d69b9a1
sailaja2003 <sailajapolicharla2003@gmail.com>
2022-11-29 17:13:16 +0530

    broker files

---
+++ broker/broker2.py
@@ -0,0 +1,7 @@
+import socket
+from time import sleep
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while 1:
+    socket.sendto(b'hello', ('127.0.0.1', 9998))
+    print('ALIVE 2')
+    sleep(1)
