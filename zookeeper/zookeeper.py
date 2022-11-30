ac315e497cbb3e7a9c626f7b65f98294c7d87323
HaneaSeemein <haniya23seemein@gmail.com>
2022-11-30 14:02:27 +0530

    recognising the dead brokers

--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -1,5 +1,6 @@
 import socket
 import select
+import time
 path = "Big Data/Brokers"
 leader = [0,0,0,0]
 activepartitions=[0,0,0,0]
@@ -24,6 +25,18 @@ def switch_leader(failedbroker):
                 nextbroker =(nextbroker+1)%4
             leader[i]=nextbroker
     update()
+def alive(socket):
+    if(socket==1):
+        socket1_alive = True
+    elif(socket==2):
+        socket2_alive = True
+    elif(socket==3):
+        socket3_alive = True
+def dead():
+    time.sleep(5)
+    socket1_alive = False
+    socket2_alive = False
+    socket3_alive = False
 
 socket_1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -40,27 +53,23 @@ poller.register(socket_3, select.POLLIN)
 init_leader()
 while 1:
     evts = poller.poll(5000)
+    dead()
     for sock, evt in evts:
         if evt and select.POLLIN:
             if sock == socket_1.fileno():
                 data1 = socket_1.recvfrom(4096)
                 if data1:
                     print("Broker1 Alive")
-                elif not data1:
-                    print("Broker1 Dead")
-                    switch_leader(1)
+                    alive(1)
+
             if sock == socket_2.fileno():
                 data2 = socket_2.recvfrom(4096)
                 if data2:
                     print("Broker2 Alive")
-                elif not data2:
-                    print("Broker2 Dead")
-                    switch_leader(2)
+
             if sock == socket_3.fileno():
                 data3 = socket_3.recvfrom(4096)
                 if data3:
                     print("Broker3 Alive")
-                elif not data3:
-                    print("Broker3 Dead")
-                    switch_leader(3)
+
             
