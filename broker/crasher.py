e746e85f5242878f6235ae993f3639e6d13872b2
HaneaSeemein <haniya23seemein@gmail.com>
2022-12-01 01:56:49 +0530

    chuck crasher

--- broker/crasher.py
+++
@@ -1,9 +0,0 @@
-def main():
-    kill = [0,-1,-1,-1]
-    tokill1 = int(input("Enter the broker to kill"))
-    tokill2 = int(input("Enter the another broker to kill(if any)"))
-    t1 = int(input("Enter the timeout for the first kill"))
-    t2 = int(input("Enter the timeout for the second kill"))
-    kill[tokill1]=t1
-    kill[tokill2]=t2
-    return kill
