fcb880256a94d2eeb9bcffca52982353c9fd6bd0
HaneaSeemein <haniya23seemein@gmail.com>
2022-11-30 14:45:37 +0530

    recognising the dead brokers-part 2

--- zookeeper/zookeeper.py
+++ zookeeper/zookeeper.py
@@ -4,7 +4,18 @@ import time
 path = "Big Data/Brokers"
 leader = [0,0,0,0]
 activepartitions=[0,0,0,0]
+socket_1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+socket_1.bind(('127.0.0.1', 9997))
+socket_2.bind(('127.0.0.1', 9998))
+socket_3.bind(('127.0.0.1', 9999))
+
+poller = select.poll()
+poller.register(socket_1, select.POLLIN)
+poller.register(socket_2, select.POLLIN)
+poller.register(socket_3, select.POLLIN)
 def update():
     k=0
     print(leader)
@@ -25,35 +36,37 @@ def switch_leader(failedbroker):
                 nextbroker =(nextbroker+1)%4
             leader[i]=nextbroker
     update()
+
+init_leader()
+timer1=time.time()
+timer2=time.time()
+timer3=time.time()
 def alive(socket):
     if(socket==1):
+        # restart_timer1
+        timer1=time.time()
         socket1_alive = True
     elif(socket==2):
+        # restart_timer2
+        timer2=time.time()
         socket2_alive = True
     elif(socket==3):
+        # restart_timer3
+        timer3=time.time()
         socket3_alive = True
-def dead():
-    time.sleep(5)
-    socket1_alive = False
-    socket2_alive = False
-    socket3_alive = False
 
-socket_1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-socket_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-socket_1.bind(('127.0.0.1', 9997))
-socket_2.bind(('127.0.0.1', 9998))
-socket_3.bind(('127.0.0.1', 9999))
-
-poller = select.poll()
-poller.register(socket_1, select.POLLIN)
-poller.register(socket_2, select.POLLIN)
-poller.register(socket_3, select.POLLIN)
-init_leader()
 while 1:
     evts = poller.poll(5000)
-    dead()
+    if((timer1-time.time())>10):
+        switch_leader(1)
+        timer1=0
+    if(timer2-time.time()>10):
+        switch_leader(2)
+        timer2=0
+    if(timer3-time.time()>10):
+        switch_leader(3)
+        timer3=0
     for sock, evt in evts:
         if evt and select.POLLIN:
             if sock == socket_1.fileno():
@@ -66,10 +79,11 @@ while 1:
                 data2 = socket_2.recvfrom(4096)
                 if data2:
                     print("Broker2 Alive")
-
+                    alive(2)
             if sock == socket_3.fileno():
                 data3 = socket_3.recvfrom(4096)
                 if data3:
                     print("Broker3 Alive")
+                    alive(3)
 
             
