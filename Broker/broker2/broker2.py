ff6fcc9164cba4eace849e3791c8d25c732c5ce4
Rohan N <rohantotherescue@gmail.com>
2022-11-30 17:16:59 +0530

    Docker add-ons

--- Broker/broker2/broker2.py
+++ Broker/broker2/broker2.py
@@ -2,6 +2,6 @@ import socket
 from time import sleep
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 while 1:
-    socket.sendto(b'hello', ('127.0.0.1', 9998))
+    socket.sendto(b'hello', ('172.22.0.4', 9998))
     print('ALIVE 2')
     sleep(1)
